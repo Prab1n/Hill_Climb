9ffd25ce44247786839d7918d421ab200e7c8fc3
Prab1n <prabin.viper@gmail.com>
2023-10-01 12:32:43 +0545

    first commit

---
+++ Assets/CameraController.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Transform target;
+
+    private void FixedUpdate()
+    {
+        Vector3 newPosition = target.position;
+        newPosition.z = -10;
+
+        transform.position = newPosition;
+    }
+    
+
+}
